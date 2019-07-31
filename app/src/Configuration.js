5639a733fafa0e46ee3385c75de3e32f86034adb
Mark Solters <msolters@gmail.com>
2019-07-30 23:29:43 -0400

    Add reference to doc library

--- app/src/Configuration.js
+++ app/src/Configuration.js
@@ -14,9 +14,11 @@ module.exports = {
       name: 'app-top',
       doc: `
 # Welcome!
-\`app-top\` is a tool for creating relational documentation.  It allows you to write markdown documentation, which can then create dependency links to other pieces of documentation.
+\`app-top\` is a tool for creating relational documentation.  It allows you to write markdown documentation, which can then create dependency links to other pieces of documentation.  The documentation is provided in a simple text file format.
 
-\`app-top\` will then render graphical networks depicting the dependencies of your system.  These networks can be analyzed, rearranged, and filtered by depth or direction.
+For instance, see the how this library is [defined](https://github.com/msolters/app-top/tree/master/app/docs).
+
+\`app-top\` will render draggable graphical networks depicting the dependencies of your system.  These networks can be analyzed, rearranged, and filtered by depth or direction.
 
 It is intended to be used to document e.g. the relationships between core areas of a business product (such as what may appear on a status page), and myriad backend dependencies that such products entail: DNS records correctly configured, specific microservices running healthily, other network constructs such as endpoints and so on.
 
